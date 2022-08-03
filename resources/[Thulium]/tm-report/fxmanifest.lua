76bad5c8684863fc7a15d41e51f713a7d6506cbe
PenT1x <68808737+PenT1x@users.noreply.github.com>
2022-08-03 23:50:02 +0200

    Updated text

--- resources/[Thulium]/tm-report/fxmanifest.lua
+++ resources/[Thulium]/tm-report/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Thulium.dev'
 description 'Takes a screenshot of potential modders and sends it to a manual review'
-version '0.1.4'
+version '0.1.5'
 
 client_script 'client.lua'
 server_scripts {
